7e490442dd72354fd5744f8b474603e777f97821
Alan Koger <adkoge@gmail.com>
2024-10-30 12:56:28 -0400

    Update vite config to use tailwind and autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind, autoprefixer],
+    },
   },
 });
