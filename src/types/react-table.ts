1d73384f737534e62439913c67d45ef234b96c86
Alan Koger <adkoge@gmail.com>
2024-10-31 08:47:19 -0400

    Add types for TableHeader

---
+++ src/types/react-table.ts
@@ -0,0 +1,22 @@
+import { Header, HeaderGroup, Row } from "@tanstack/react-table";
+
+export interface ColumnMetaOptions {
+  flexJustify?: "center" | "start" | "end" | "inherit";
+  textAlign?: "center" | "left" | "right" | "inherit";
+}
+
+export interface TableBodyRowProps<TData> {
+  row: Row<TData>;
+}
+
+export interface TableHeaderProps<TData> {
+  headerGroups: HeaderGroup<TData>[];
+}
+
+export interface TableHeaderRowProps<TData> {
+  headerGroup: HeaderGroup<TData>;
+}
+
+export interface HeaderCellProps<TData> {
+  header: Header<TData, unknown>;
+}
